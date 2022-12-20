b360397ff22dd192002f736f4e189f279fb2cdb8
Zortaniac <github@zortaniac.org>
2022-12-20 15:10:10 +0100

    Add day 15 part 2

--- day15.go
+++ day15.go
@@ -46,27 +46,50 @@ func day15() {
 		fmt.Println(err)
 	}
 
-	rowToCheck := 2000000
-	checked := make(map[int]struct{})
-	for sensor, _ := range sensors {
-		dist := distances[sensor]
-		for y := 0; y < dist; y++ {
-			if sensor.y-dist+y == rowToCheck || sensor.y+dist-y == rowToCheck {
-				// check upper
-				for x := 0; x <= y; x++ {
-					if _, exists := beacons[pos{x: sensor.x - x, y: rowToCheck}]; !exists {
-						checked[sensor.x-x] = struct{}{}
+	{
+		rowToCheck := 2000000
+		checked := make(map[int]struct{})
+		for sensor := range sensors {
+			dist := distances[sensor]
+			if sensor.y-rowToCheck > dist {
+				continue
+			}
+			y := rowToCheck - sensor.y
+			// check upper
+			for x := -(dist - Abs(y)); x <= (dist - Abs(y)); x++ {
+				if _, exists := beacons[pos{x: sensor.x + x, y: rowToCheck}]; !exists {
+					checked[sensor.x+x] = struct{}{}
+				}
+			}
+		}
+		fmt.Println(len(checked))
+	}
+	{
+		boundary := 4000000
+		for y := 0; y <= boundary; y++ {
+			for x := 0; x <= boundary; x++ {
+				found := false
+				for sensor := range sensors {
+					distSensor := tDist(pos{x: x, y: y}, sensor)
+					if distSensor <= distances[sensor] {
+						x = xMove(sensor, y, distances[sensor])
+						found = true
+						break
 					}
-					if _, exists := beacons[pos{x: sensor.x + x, y: rowToCheck}]; !exists {
-						checked[sensor.x+x] = struct{}{}
-					}
+				}
+				if x <= boundary && !found {
+					fmt.Println(x*4000000 + y)
+					return
 				}
 			}
 		}
 	}
-	fmt.Println(len(checked))
 }
 
 func tDist(a pos, b pos) int {
 	return Abs(a.x-b.x) + Abs(a.y-b.y)
+}
+
+func xMove(p pos, y int, d int) int {
+	return d - Abs(p.y-y) + p.x
 }
